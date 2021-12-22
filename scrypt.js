a7755473b99627c9792f732e7c5e0c55273a40c8
Misha Shmidt <mishashmidt0@gmail.com>
2021-12-23 00:14:33 +0300

    feat: add scrypt filter

--- scrypt.js
+++ scrypt.js
@@ -1,5 +1,29 @@
 function app() {
+    const button = document.querySelectorAll('.button');
+    const cards = document.querySelectorAll('.card')
+    console.log(cards)
+    function filter(category, items) {
+        items.forEach(item => {
+            console.log(item.classList.contains(category))
+            const isItemFiltered = !item.classList.contains(category);
+            const isShowAll = category.toLowerCase() === 'all';
 
+            if (isItemFiltered && !isShowAll) {
+                item.classList.add('hide')
+            } else {
+                item.classList.remove('hide')
+            }
+        })
+    }
+
+
+    button.forEach((el) => {
+        el.addEventListener('click', () => {
+            const currentCategory = el.dataset.filter;
+            filter(currentCategory, cards)
+
+        })
+    })
 }
 
 
